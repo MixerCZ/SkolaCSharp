8d19b2428e10610e797e3e96a202530915154dd6
Lukas Dolejs <st024210@student.cichnovabrno.cz>
2024-01-26 11:53:46 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
