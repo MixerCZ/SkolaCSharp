3c1af5e9f19200e8b1d26d57cac173b5ec359d1e
Lukas Dolejs <st024210@student.cichnovabrno.cz>
2024-01-26 12:14:10 +0100

    opakování cyklů

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Skola
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int cislo;
+        int counter = 0, max = 0, sum = 0, min = 2100000000;
+        double avg = 0;
+        
+        do
         {
-        }
+            Console.Write($"Zadej {counter+1}. číslo: ");
+            cislo = int.Parse(Console.ReadLine());
+            if (cislo != 0)
+            {
+                if(cislo < min)
+                    min = cislo;
+
+                if (cislo > max)
+                    max = cislo;
+
+                sum += cislo;
+                counter++;
+                avg = sum / (double) counter;
+            }
+            
+        } while (cislo != 0);
+
+        Console.WriteLine($"Minimum je {min}, maximum je {max}, průměr je {avg}. Zadal si {counter} čísel.");
+        Console.ReadKey();
     }
 }
+
