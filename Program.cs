59ed71ee0cd76d63072f2245d10b0d8684e5af4d
Lukas Dolejs <st024210@student.cichnovabrno.cz>
2024-02-13 11:30:35 +0100

    další hodina

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ class Program
     static void Main()
     {
         //Test();
-        Samohlasky();
+        //Samohlasky();
         //Pocitadlo();
+        //MinMax();
+        ZadejDokud();
     }
 
     static void Test()
@@ -71,5 +73,76 @@ class Program
         Console.WriteLine($"Minimum je {min}, maximum je {max}, průměr je {avg}. Zadal si {counter} čísel.");
         Console.ReadKey();
     }
+
+    static void NacteniZnamek()
+    {
+        Console.Write("Zadej velikost pole: ");
+        int[] arr = new int[int.Parse(Console.ReadLine())];
+        int sum = 0;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            do
+            {
+                Console.Write($"Zadej {i + 1}. hodnotu: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            } while (arr[i] > 0 && arr[i] <= 5);
+            sum += arr[i];
+        }
+
+        double avg = (double)sum / (double)arr.Length;
+        Console.WriteLine("Průměr známek je " + avg);
+    }
+
+    static void MinMax()
+    {
+        int min = int.MaxValue, max = int.MinValue, sum = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            Console.Write($"Zadej {i + 1}. hodnotu: ");
+            int c = int.Parse(Console.ReadLine());
+
+            if (c > max) max = c;
+            if (c < min) min = c;
+            sum += c;
+        }
+
+        Console.WriteLine("Minimum je " + min + ", maximum je " + max + ", součet je " + sum + ".");
+        Console.ReadLine();
+    }
+
+    static void ZadejDokud()
+    {
+        int c = 0;
+        int[] arr = new int[100];
+        int i = 0;
+        do
+        {
+            Console.Write("Zadej: ");
+            c = int.Parse(Console.ReadLine());
+            arr[i] = c;
+            i++;
+        } while (c != 0);
+
+        avg(arr);
+    }
+
+    static void avg(int []arr)
+    {
+        int sum = 0;
+        int c = 0;
+        foreach(int i in arr)
+        {
+            if (i < 10 && i > 0) {
+                sum += i;
+                c++;
+            };
+        }
+
+        double avg = (double)sum / (double)c;
+        Console.WriteLine("Průměr je " + avg);
+        Console.ReadLine();
+    }
+
 }
 
